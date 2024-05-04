20697515c98f1bf1e00d6fd2b1cdcc705d7c9eb1
Rishu jha <jharishu796@gmail.com>
2024-05-04 22:33:21 +0530

    cross origin resouse sharing configured in index.js file for cookie handling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/index.js");
@@ -7,7 +8,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: `${process.env.CLIENT_URL}/`,
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/auth", routes.authRouter);
