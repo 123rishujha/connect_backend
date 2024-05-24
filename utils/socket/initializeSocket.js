c2cf79d7ea49cc213b2f5593580df9fdf7c3505b
123rishujha <jharishu796@gmail.com>
2024-05-24 08:05:58 +0530

    get active users in textroom and video room socket events created

--- utils/socket/initializeSocket.js
+++ utils/socket/initializeSocket.js
@@ -1,21 +1,39 @@
 const initializeSocket = (io) => {
-  const onlineUser = {};
+  const activeUser = {
+    onlineUser: {},
+    textRoom: {},
+    videoRoom: {},
+  };
 
   io.on("connection", (socket) => {
     console.log("User connected", socket.id);
-    onlineUser[socket.id] = true;
+    activeUser.onlineUser[socket.id] = true;
     // notify all the sockets that new user come online
-    io.emit("onlineUsers", { onlineUsers: Object.keys(onlineUser).length });
+    io.emit("onlineUsers", {
+      onlineUsers: Object.keys(activeUser.onlineUser).length,
+      textRoom: Object.keys(activeUser.textRoom).length,
+      videoRoom: Object.keys(activeUser.videoRoom).length,
+    });
     //get active users;
     socket.on("getOnlineUser", () => {
       socket.emit("onlineUsers", {
-        onlineUsers: Object.keys(onlineUser).length,
+        onlineUsers: Object.keys(activeUser.onlineUser).length,
+        textRoom: Object.keys(activeUser.textRoom).length,
+        videoRoom: Object.keys(activeUser.videoRoom).length,
       });
     });
 
     socket.on("disconnect", () => {
-      delete onlineUser[socket.id];
-      io.emit("onlineUsers", { onlineUsers: Object.keys(onlineUser).length });
+      delete activeUser.onlineUser[socket.id];
+      if (activeUser.textRoom[socket.id]) delete activeUser.textRoom[socket.id];
+      if (activeUser.videoRoom[socket.id])
+        delete activeUser.videoRoom[socket.id];
+
+      io.emit("onlineUsers", {
+        onlineUsers: Object.keys(activeUser.onlineUser).length,
+        textRoom: Object.keys(activeUser.textRoom).length,
+        videoRoom: Object.keys(activeUser.videoRoom).length,
+      });
       console.log("user disconnected", socket.id);
     });
   });
